0c48e274cc7056c683e27876caacab812f2f5789
Robed Beauvile <59368349+beauvilerobed@users.noreply.github.com>
2020-03-20 10:26:21 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,7 @@
 </head>
 <body>
 <div class="container">
-<h1>Welcome to Broken Rock Paper Scissors</h1>
-<p><strong>Note:</strong> This sample code is only
-partially done and serves only as a starting point for the assignment.
-</p>
+<h1>Welcome to Rock Paper Scissors</h1>
 <p>
 <a href="login.php">Please Log In</a>
 </p>
